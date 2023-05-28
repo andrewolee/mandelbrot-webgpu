49484e0f796afb279797218aba87e7e1598d2292
Andrew Lee <132124185+andrewolee@users.noreply.github.com>
2023-05-27 22:56:48 -0700

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
-//import vertexShader from
-//import fragmentShader from
+const shaders = `
+struct VertexOut
+{
+  @builtin(position) position : vec4f,
+  @location(0) color : vec4f
+}
+
+@vertex
+fn vertex_main(@location(0) position: vec4f, @location(1) color: vec4f) -> VertexOut
+{
+  var output : VertexOut;
+  output.position = position;
+  output.color = color;
+  return output;
+}
+
+@fragment
+fn fragment_main(fragData: VertexOut) -> @location(0) vec4f
+{
+  return fragData.color;
+}
+`;
 
 async function main() {
   if (!navigator.gpu) {
@@ -12,6 +32,19 @@ async function main() {
   }
 
   const device = await adapter.requestDevice();
+
+  const shaderModule = device.createShaderModule({
+    code: shaders,
+  });
+
+  const canvas = document.getElementById("mandelbrot") as HTMLCanvasElement;
+  const context = canvas.getContext("webgpu");
+  
+  context?.configure({
+    device: device,
+    format: navigator.gpu.getPreferredCanvasFormat(),
+    alphaMode: "premultiplied"
+  });
 }
 
 main();
