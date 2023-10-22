4f39481c48101f5743229dbf03b317e9ecb0dea3
Andrew Lee <132124185+andrewolee@users.noreply.github.com>
2023-10-21 21:34:56 -0700

    wip

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,60 @@ async function init() {
     format: navigator.gpu.getPreferredCanvasFormat(),
     alphaMode: "premultiplied",
   });
+
+  const vertices = new Float32Array([
+    0.0, 0.6, 0, 1, 1, 0, 0, 1, -0.5, -0.6, 0, 1, 0, 1, 0, 1, 0.5, -0.6, 0, 1,
+    0, 0, 1, 1,
+  ]);
+
+  const vertexBuffer = device.createBuffer({
+    size: vertices.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+  });
+
+  device.queue.writeBuffer(vertexBuffer, 0, vertices, 0, vertices.length);
+
+  const vertexBuffers = [
+    {
+      attributes: [
+        {
+          shaderLocation: 0,
+          offset: 0,
+          format: "float32x4",
+        },
+        {
+          shaderLocation: 1,
+          offset: 16,
+          format: "float32x4",
+        },
+      ],
+      arrayStride: 32,
+      stepMode: "vertex",
+    },
+  ];
+
+  const pipelineDescriptor = {
+    vertex: {
+      module: shaderModule,
+      entryPoint: "vertex_main",
+      buffers: vertexBuffers,
+    },
+    fragment: {
+      module: shaderModule,
+      entryPoint: "fragment_main",
+      targets: [
+        {
+          format: navigator.gpu.getPreferredCanvasFormat(),
+        },
+      ],
+    },
+    primitive: {
+      topology: "triangle-list",
+    },
+    layout: "auto",
+  };
+
+  const renderPipeline = device.createRenderPipeline(pipelineDescriptor);
 }
 
 init();
